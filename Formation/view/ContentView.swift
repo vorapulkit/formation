47cfe70825b63c5261d7fa3422618018920717df
Pulkit <pulkitde@gmail.com>
2020-02-18 11:05:10 +0530

    Initial Setup of Formation

---
+++ Formation/view/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Formation
+//
+//  Created by Pulkit Vora on 17/02/20.
+//  Copyright © 2020 Pulkit Vora. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Formation")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
